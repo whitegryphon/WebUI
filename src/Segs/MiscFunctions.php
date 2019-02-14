0735b7e0131b24ce3cbd175d2ece47e8fd494342
White Gryphon <whitegryphon@gmail.com>
2019-02-14 07:10:54 -0500

    Update functions

--- src/Segs/MiscFunctions.php
+++ src/Segs/MiscFunctions.php
@@ -24,15 +24,6 @@ error_reporting(E_ALL);
         // //     //die('Functions');
         // // }
 
-        function debug_to_console( $data ) {
-            if ( is_array( $data ) ) {
-                $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
-            } else {
-                $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
-            }
-            echo $output;
-        }
-
         function generate_salt($length = 16) {
             $possible_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678';
             $rand_string = '';
@@ -43,11 +34,12 @@ error_reporting(E_ALL);
         }
 
         function hash_pass($plaintext_pass, $salt) {
+            $test = "Whut?";
             try {
-                $test = hash('sha256', $plaintext_pass . $salt, true);
-                //$test = hash('sha256', $plaintext_pass . $salt);
+                //$test = hash('sha256', $plaintext_pass . $salt, true);
+                $test = hash('sha256', $plaintext_pass . $salt);
             } catch (Exception $e) {
-                $test = null;
+                $test = "Whut??";
             }
             return $test;
         }
@@ -60,24 +52,31 @@ error_reporting(E_ALL);
             $username = substr($escapedUsername, 0, strlen($escapedUsername));
             $password = substr($escapedPassword, 0, strlen($escapedPassword));
             $sample_salt = $this->generate_salt();
+            $password = "Password";
             $return_value = new RETURN_TYPE();
             $hashed_pass_bytearr = $this->hash_pass($password, $sample_salt);
-            if($hashed_pass_bytearr !== null){
-                $db_conn = new DatabaseConnection($dbhost, $dbuser, $dbpass, $accdb);
-                if($stmt = $db_conn->PrepareStatement("INSERT INTO accounts(username, passw, salt) VALUES(?, ?, ?)")) {
-                    $stmt->bind_param('sss', $username, $hashed_pass_bytearr, $sample_salt);
-                    if(!$stmt->execute()) {
-                        $return_value->message = "User creation failed. Error: " . $db_conn->errno;
-                        $return_value->value = 1;
-                    } else {
-                        $return_value->return_message[] = "User '$username' was succesfully created!";
-                        $return_value->value = 0;
-                    }
-            } else {
-                $return_value->message = "Failed to connect to MySQL. Error: " . $db_conn->errno;
-                $return_value->value = 1;
+            $db_conn = new DatabaseConnection($dbhost, $dbuser, $dbpass, $accdb);
+
+            //$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $accdb);
+            //if ($mysqli->connect_errno)
+            //{ 
+            //    $return_value->message = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            //    $return_value->value = 1;
+            //    $mysqli->close();
+            //    return $return_value;
+            //}
+            if($stmt = $db_conn->PrepareStatement("INSERT INTO accounts(username, passw, salt) VALUES(?, ?, ?)")) {
+                $stmt->bind_param('sss', $username, $hashed_pass_bytearr, $sample_salt);
+                if(!$stmt->execute()) {
+                    $return_value->message = "User creation failed! " . $mysqli->errno;
+                    $return_value->value = 1;
+                } else {
+                    $return_value->return_message[] = "User '$username' was succesfully created!";
+                    $return_value->value = 0;
+                }
+            // //     $mysqli->close();
+                return $return_value;
             }
-            return $return_value;
         }
 
         public function FetchChat()

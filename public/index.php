8ac290bd47c66187418c0df2a802815aec444106
White Gryphon <whitegryphon@gmail.com>
2019-02-19 15:43:34 -0500

    Code cleanup

--- public/index.php
+++ public/index.php
@@ -73,7 +73,14 @@
                 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
                     <div class="container-fluid">
                         <div class="navbar-wrapper">
-                            <a class="navbar-brand" href="#"><?php echo $site_navbar_title; ?></a>
+                            <a class="navbar-brand" href="?page=dashboard">
+                                <?php
+                                    echo $site_navbar_title; 
+                                    if(isset($_SESSION['isAuthenticated']) && isset($_SESSION['username'])) {
+                                        echo " - " . $_SESSION['username']; 
+                                    }
+                                ?>
+                            </a>
                         </div>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="sr-only">Toggle navigation</span>
@@ -82,43 +89,13 @@
                             <span class="navbar-toggler-icon icon-bar"></span>
                         </button>
                         <div class="collapse navbar-collapse justify-content-end">
-                            <form class="navbar-form">
-                                <div class="input-group no-border">
-                                    <input type="text" value="" class="form-control" placeholder="Search...">
-                                    <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                                        <i class="material-icons">search</i>
-                                        <div class="ripple-container"></div>
-                                    </button>
-                                </div>
-                            </form>
                             <ul class="navbar-nav">
                             <?php if(isset($_SESSION['isAuthenticated'])) { ?>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="material-icons">notifications</i>
-                                        <span class="notification">5</span>
-                                        <p class="d-lg-none d-md-block">Some Actions</p>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                        <a class="dropdown-item" href="#">Notifications are not implemented.</a>
-                                        <a class="dropdown-item" href="#">These are currently static.</a>
-                                        <a class="dropdown-item" href="#">SEGS Server has been updated to version 0.6.1 Outbreak</a>
-                                        <a class="dropdown-item" href="#">Another Notification</a>
-                                        <a class="dropdown-item" href="#">Another One</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="?page=user">
-                                        <i class="material-icons">person</i>
-                                        <p class="d-lg-none d-md-block">Account</p>
-                                    </a>
-                                </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#logout" onclick="doLogout();">
                                         <i class="fas fa-sign-out"></i>Log Out
                                     </a>
                                 </li>
-
                             <?php } else { ?>
                                 <li class="nav-item ">
                                     <a class="nav-link" href="#modal-login" data-toggle="modal" data-target="#modal-login">
